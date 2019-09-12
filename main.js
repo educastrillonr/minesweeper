734c1ded8b26eb9008878e06a8891c6d72369584
Edu <educastrillonr@gmail.com>
2019-09-12 18:42:09 +0100

    some refactor

--- main.js
+++ main.js
@@ -3,10 +3,15 @@ const get = id => {
 };
 
 class Cell {
-  constructor(isAMine, coord) {
+  constructor(isAMine, x, y) {
     this._isAMine = isAMine;
-    this._coord = [...coord];
+    this._coord = {
+      x: x,
+      y: y
+    };
     this._cellDom = null;
+    this._mines = 0;
+    this._neighbours = [];
   }
 
   get isAMine() {
@@ -21,40 +26,76 @@ class Cell {
     return this._cellDom;
   }
 
+  set neighbours(newCell) {
+    this._neighbours.push(newCell);
+  }
+
   set isAMine(newValue) {
     this._isAMine = newValue;
   }
 
+  set mines(newValue) {
+    this._mines = newValue;
+  }
+
   insertCell = row => {
-    this._cellDom = row.insertCell(this._coord[1]);
-    this._cellDom.onclick = this.handleCellClick;
+    this._cellDom = row.insertCell(this._coord.y);
   };
 
-  handleCellClick = () => {
-    // this._isAMine
-    //   ? alert("Game Over")
-    //   : // this._cellDom.onclick = null;
-    //     null;
+  handleCellClick = cellArray => {
     if (this._isAMine) {
-      this._cellDom.onclick = null;
+      showMines(cellArray);
       alert("Game Over");
+    } else {
+      this.cellDom.className = "clicked";
+      this.cellDom.onclick = null;
+      this.cellDom.innerHTML = this._mines > 0 ? this._mines : " ";
+      if (this._mines === 0) {
+        for (let index = 0; index < this._neighbours.length; index++) {
+          if (this._neighbours[index].cellDom.innerHTML == "") {
+            this._neighbours[index].handleCellClick(cellArray);
+          }
+        }
+      }
+      if (isComplete(cellArray)) {
+        alert("YOU WIN");
+      }
     }
   };
 }
-const getGrid = (rows, columns, mines) => {
-  let cellArray = [];
-  for (let i = 0; i < rows; i++) {
-    let row = get("grid").insertRow(i);
-    cellArray.push([]);
-    for (let j = 0; j < columns; j++) {
-      const cell = new Cell(false, [i, j]);
-      cell.insertCell(row);
-      cellArray[i].push(cell);
+
+const isComplete = cellArray => {
+  for (let index = 0; index < cellArray.length; index++) {
+    for (let index2 = 0; index2 < cellArray[index].length; index2++) {
+      let cell = cellArray[index][index2];
+      if (!cell.isAMine && cell.cellDom.innerHTML == "") {
+        return false;
+      }
     }
   }
-  console.log(cellArray);
+  return true;
+};
+
+const getGrid = (rows, columns, mines) => {
+  let cellArray = [];
+  for (let x = 0; x < rows; x++) {
+    let row = get("grid").insertRow(x);
+    cellArray.push([]);
+    for (let y = 0; y < columns; y++) {
+      const cell = new Cell(false, x, y);
+      cell.insertCell(row);
+      cell.cellDom.onclick = () => cell.handleCellClick(cellArray);
+      cellArray[x].push(cell);
+    }
+  }
+  get(
+    "game-info"
+  ).innerHTML = `Game, W: ${columns}, H: ${rows}, Mines: ${mines}`;
 
   getMines(cellArray, mines);
+  getResults(cellArray);
+
+  return cellArray;
 };
 
 const getMines = (cellArray, mines) => {
@@ -65,11 +106,41 @@ const getMines = (cellArray, mines) => {
   }
 };
 
-const revealMines = cellArray => {
+const getResults = cellArray => {
   for (let index = 0; index < cellArray.length; index++) {
     for (let index2 = 0; index2 < cellArray[index].length; index2++) {
       let cell = cellArray[index][index2];
+      if (!cell.isAMine) {
+        generateNeighbours(cell, cellArray);
+      }
+    }
+  }
+};
 
+const generateNeighbours = (cell, cellArray) => {
+  const firstRow = Math.max(cell.coord.x - 1, 0);
+  const lastRow = Math.min(cell.coord.x + 1, cellArray.length - 1);
+  const firstColumn = Math.max(cell.coord.y - 1, 0);
+  const lastColumn = Math.min(cell.coord.y + 1, cellArray[0].length - 1);
+  let mines = 0;
+
+  for (let index = firstRow; index <= lastRow; index++) {
+    for (let index2 = firstColumn; index2 <= lastColumn; index2++) {
+      let neighbour = cellArray[index][index2];
+      cell.neighbours = neighbour;
+      if (neighbour.isAMine) {
+        mines++;
+      }
+    }
+  }
+  cell.mines = mines;
+};
+
+const showMines = cellArray => {
+  for (let index = 0; index < cellArray.length; index++) {
+    for (let index2 = 0; index2 < cellArray[index].length; index2++) {
+      let cell = cellArray[index][index2];
+      cell.cellDom.onclick = null;
       if (cell.isAMine) {
         cell.cellDom.className = "mine";
       }
@@ -77,9 +148,25 @@ const revealMines = cellArray => {
   }
 };
 
-resetGrid = () => {
-  get("grid").innerHTML = "";
-  getGrid(15, 15, 50);
+const getRandomCell = cellArray => {
+  let x = Math.floor(Math.random() * cellArray.length);
+  let y = Math.floor(Math.random() * cellArray[0].length);
+  return cellArray[x][y];
 };
 
-getGrid(15, 15, 50);
+const generateClue = cellArray => {
+  let cell = getRandomCell(cellArray);
+
+  while (cell.isAMine || cell.cellDom.innerHTML === 0) {
+    cell = getRandomCell(cellArray);
+  }
+  cell.handleCellClick(cellArray);
+};
+
+const resetGrid = () => {
+  get("grid").innerHTML = "";
+  let cellArray = getGrid(15, 15, 40);
+  generateClue(cellArray);
+};
+
+resetGrid();
